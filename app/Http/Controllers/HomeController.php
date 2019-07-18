345224f75e65400c5bb98b9f3350d7bd942f4307
Fabrizio Romanelli <fabev6@gmail.com>
2019-07-18 22:02:01 +0200

    Ho fatto le cose

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,23 +8,24 @@ use Intervention\Image\ImageManagerStatic;
 class HomeController extends Controller
 {
     public function generate(Request $request){
-        $img = ImageManagerStatic::make(public_path('img/base.jpeg'));
-        $img->text($request->home, 305, 930, function($font) {
+        $img = ImageManagerStatic::make(public_path('img/convocati.png'));
+        $img->text("CONVOCATI", 86, 130, function($font) {
             $font->file(public_path('fonts/Roboto.ttf'));
-            $font->size(140);
-            $font->color('#e1e1e1');
-            $font->align('center');
+            $font->size(130);
+            $font->color('#ffffff');
+            $font->align('left');
             $font->valign('bottom');
             $font->angle(0);
         });
-        $img->text($request->away, 435, 930, function($font) {
+        $img->insert(public_path('img/divider.png'), 'top-left', 60, 180);
+        $img->text("PORTIERI", 86, 230, function($font) {
             $font->file(public_path('fonts/Roboto.ttf'));
-            $font->size(140);
-            $font->color('#e1e1e1');
-            $font->align('center');
+            $font->size(50);
+            $font->color('#ffffff');
+            $font->align('left');
             $font->valign('bottom');
             $font->angle(0);
         });
-        $img->save(public_path('img/base.jpg'));
+        $img->save(public_path('img/convocati.jpg'));
     }
 }
